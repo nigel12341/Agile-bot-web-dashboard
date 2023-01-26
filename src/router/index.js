19b2040a423e7c97143f8ca5d54a10085394bae1
Nigel Christiaans <nigel.christiaans@hva.nl>
2023-01-27 00:26:59 +0100

    Added userview for data not linked to a server

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import StatsComponent from "@/components/ServerOverviewComponent.vue";
 import logOutComponent from "@/components/logOutComponent.vue";
 import ServerView from "@/views/ServerView.vue";
+import UserView from "@/views/UserView.vue";
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     path: '/logout',
     name: 'logout',
     component: logOutComponent
+  },
+  {
+    path: '/userView',
+    name: 'userView',
+    component: UserView
   }
 ]
 
---
+++ src/views/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Your personal data not related to one server.</h1>
+<transscript-component/>
+</template>
+
+<script>
+import TransscriptComponent from "@/components/transscriptComponent";
+export default {
+  name: "UserView",
+  components: {TransscriptComponent},
+}
+</script>
+
+<style scoped>
+
+</style>
